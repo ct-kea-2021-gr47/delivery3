732e72c81f53f3d0cd914984723001a2a730edd0
hexmonkey-3 <54470666+hexmonkey-3@users.noreply.github.com>
2022-05-03 18:06:54 +0200

    Update main.py
    import platform,os

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # yolo
+
+import os
+import platform
+
+#               What os is in use
+if platform.system() == "Linux":
+    cl='clear'
+if platform.system() == "Windows":
+    cl='cls'
+if platform.system() == "Darwin":
+    cl='clear'
+
+def clear():  
+    os.system(cl)
